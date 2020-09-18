705a3ec5e516a01280491efe6a70553d57009ce5
PLR <51248199+plredmond@users.noreply.github.com>
2020-09-17 17:37:05 -0700

    add a refinement on a class

--- lib/Foo.hs
+++ lib/Foo.hs
@@ -11,3 +11,16 @@ import LiquidHaskell (lq)
 -- True
 [lq| x :: TRUE |]
 x = not False
+
+[lq| type Nat = {v:Int | 0 <= v} |]
+
+[lq| bar :: {n:Nat | n < 10} |]
+bar = 2
+
+class Foo a where
+    foo :: a -> Int
+{-@ foo :: a -> {n:Nat | n < 10} @-}
+
+instance Foo Bool where
+    foo True = 1
+    foo False = 11
