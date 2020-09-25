beb8ff155b37f737def921815ffe90adbab86a6d
PLR <51248199+plredmond@users.noreply.github.com>
2020-09-25 14:56:45 -0700

    add question about a failed termination check

--- lib/Confusions.hs
+++ lib/Confusions.hs
@@ -4,6 +4,41 @@ module Confusions where
 import LiquidHaskell (lq)
 import Data.Int
 
+--------------------------------------------------
+-- (7) trivial recursive function which confuses the measure
+
+-- | Measure for a list
+myLen :: [a] -> Int
+myLen ([]) = 0
+myLen ((_x:xs)) = 1 + myLen (xs)
+{-@ measure myLen @-}
+
+-- | Data structure hiding a list internally
+data Foo a = Foo [a] deriving Show
+
+-- | Measure for Foo which delegates to the measure for a list
+fooLen :: Foo a -> Int
+fooLen (Foo xs) = myLen xs
+{-@ measure fooLen @-}
+
+-- | Specification for Foo which assigns the measure to it
+{-@ data Foo [fooLen] @-}
+
+fooLen2 :: Foo a -> Int
+fooLen2 (Foo []) = 0
+fooLen2 (Foo (_x:xs)) = 1 + fooLen2 (Foo xs)
+-- Why is fooLen2 rejected?
+-- https://liquidhaskell.slack.com/archives/C54QAL9RR/p1600905279008400
+-- resulted in GH issue https://github.com/ucsd-progsys/liquidhaskell/issues/1766
+
+-- Solution:
+{-@ myLen :: _ -> {x:_ | 0 <= x} @-}
+
+-- Why: termination checker not only constrains the size of recursive component
+-- to be smaller, but also on minimum size of recursive component.
+
+--------------------------------------------------
+
 -- (6) bugs around scoping
 -- importing measures defined by quasiquoter https://github.com/ucsd-progsys/liquidhaskell/issues/1764
 -- using an alias which uses an inlined function https://github.com/ucsd-progsys/liquidhaskell/issues/1761
