f49b7985e0e0246a5bd0ab5ebfff48d787d43c83
PLR <51248199+plredmond@users.noreply.github.com>
2020-10-27 21:26:16 -0700

    move the definitions about happens before and other things to the file where the proof has been started

--- lib/Causal/CBCAST/Impl.hs
+++ lib/Causal/CBCAST/Impl.hs
@@ -1,42 +1,4 @@
-
-
 module Causal.CBCAST.Impl where
-
--- page 7/278:
---
---      "The execution of a process is a partial ordered sequence of _events_,
---      each corresponding to the execution of an indivisible action. An
---      acyclic event order, denoted ->^p, reflects the dependence of events
---      occuring at process p upon one another."
---
---      "As Lamport [17], we define the potential causality relation for the
---      system, ->, as the transitive closure of the relation defined as
---      follows:
---
---      (1) if there-exists p: e ->^p e' then e -> e'
---      (2) for-all m: send(m) -> rcv(m)"
---
--- page 8/279:
---
---      "Two types of delivery ordering will be of interest here. We define the
---      causal delivery ordering for multicast messages m and m' as follows:
---
---          m -> m' => for-all p element-of dests(m) intersect dests(m'):
---                      deliver(m) ->^p deliver(m')
---
---      CBCAST provides only th ecausal delivery ordering."
---
--- page 10/281:
---
---      "Suppose that a set of processes P communicate using only broadcasts to
---      the full set of processes in the system; that is,
---      for-all m: dests(m) = P."
---
---      "We now develop a _delivery protocol_ by which each process p receives
---      messages sent to it, delays them if necessary, and then delivers them
---      in an order consistent with causality:
---
---          m -> m' => for-all p: deliver_p(m) ->^p deliver_p(m')
 
 import Causal.CBCAST.DelayQueue
 import Causal.CBCAST.Message
