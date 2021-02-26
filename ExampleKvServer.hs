4f2019af48917b08ae4bc49cabf2f4d944d19ecc
PLR <51248199+plredmond@users.noreply.github.com>
2021-02-26 12:47:18 -0800

    working on server types

--- ExampleKvServer.hs
+++ ExampleKvServer.hs
@@ -3,17 +3,28 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Main where
 
-import qualified Data.Aeson as JSON
+import qualified Data.Aeson as Aeson
 
 import qualified Causal.CBCAST.VectorClock as CBCAST
 import qualified Causal.CBCAST.Message as CBCAST
 
-import Network.HTTP.Types.Method -- Does not resolve LH issue
 import Servant
-import Servant.API.Generic
+import Servant.API.Generic ((:-), Generic, ToServantApi)
+-- import Servant.Server.Generic
+
+-- * Types for clients
 
 type Key = String
-type Val = JSON.Value
+type Val = Aeson.Value
+
+data ClientRoutes mode = ClientRoutes
+    { kvGet :: mode :- "kv" :> Capture "key" Key :> Get '[JSON] Val
+    , kvPut :: mode :- "kv" :> Capture "key" Key :> ReqBody '[JSON] Val :> PutNoContent
+    , kvDel :: mode :- "kv" :> Capture "key" Key :> DeleteNoContent
+    }
+    deriving (Generic)
+
+-- * Types for servers
 
 data AppCast
     = Set Key Val
@@ -21,13 +32,25 @@ data AppCast
 
 type CBCAST = CBCAST.Message AppCast
 
-data Routes mode = Routes
-    { kvGet :: mode :- "kv" :> Capture "key" Key :> Get '[JSON] Val -- Triggers LH issue
-    , kvPut :: mode :- "kv" :> Capture "key" Key :> ReqBody '[JSON] Val :> PutNoContent
-    , kvDel :: mode :- "kv" :> Capture "key" Key :> DeleteNoContent
-    , cbcast :: mode :- "cbcast" :> ReqBody '[JSON] CBCAST :> PostNoContent
+data ServerRoutes mode = ServerRoutes
+    { cbcast :: mode :- "cbcast" :> ReqBody '[JSON] CBCAST :> PostNoContent
     }
     deriving (Generic)
+
+serverWaiApp :: Application
+serverWaiApp = undefined {- serve
+    (Proxy :: Proxy (ToServantApi ClientRoutes :<|> ToServantApi ServerRoutes))
+    (clientServer :<|> serverServer) -}
+  where
+    -- https://github.com/haskell-servant/servant/issues/1394
+    clientServer :: Server (ToServantApi ClientRoutes)
+    clientServer
+        =    (\key -> return undefined)
+        :<|> (\key -> return undefined)
+        :<|> (\key -> return undefined)
+    serverServer :: Server (ToServantApi ServerRoutes)
+    serverServer
+        =    (\message -> return undefined)
 
 main :: IO ()
 main = print "example not implemented"
