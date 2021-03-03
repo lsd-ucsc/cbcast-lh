2fc52908140f0fa1dda664ff0cd7eb6f4cfb370d
PLR <51248199+plredmond@users.noreply.github.com>
2021-03-03 03:42:32 -0800

    experiment setup scripts

---
+++ deploy/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+time ./kv-store-6.sh &
+time ./kv-store-7.sh &
+time ./kv-store-4.sh &
+time ./kv-store-5.sh &
+time ./kv-store-2.sh &
+time ./kv-store-3.sh &
+time ./kv-store-0.sh &
+time ./kv-store-1.sh &
--- deploy/spit.py
+++ deploy/spit.py
@@ -33,9 +33,9 @@ def randdata(fuel):
     else:
         return None
 
-get    = lambda ip,port,key,val: f'''curl {ip}:{port}/kv/{key}'''
-delete = lambda ip,port,key,val: f'''curl -X DELETE {ip}:{port}/kv/{key}'''
-put    = lambda ip,port,key,val: f'''curl -X PUT -H "Content-type: application/json" -d '{json.dumps(val)}' {ip}:{port}/kv/{key}'''
+get    = lambda ip,port,key,val: f'''curl --silent {ip}:{port}/kv/{key}'''
+delete = lambda ip,port,key,val: f'''curl --silent -X DELETE {ip}:{port}/kv/{key}'''
+put    = lambda ip,port,key,val: f'''curl --silent -X PUT -H "Content-type: application/json" -d '{json.dumps(val)}' {ip}:{port}/kv/{key}'''
 
 if __name__ == '__main__':
 
@@ -49,4 +49,4 @@ if __name__ == '__main__':
     for n in range(round(1e5)):
         req = random.choice([get,delete,put]) if ns.mut else get
         cmd = req(ns.ip,ns.port,random.choice(string.ascii_lowercase),randdata(5))
-        print(cmd)
+        print(cmd + ' > /dev/null')
