8abb0a8a08338df354663b8f61401184014bcb53
PLR <51248199+plredmond@users.noreply.github.com>
2021-03-18 16:45:03 -0700

    set up authentication resources

--- deploy/net-aws.nix
+++ deploy/net-aws.nix
@@ -4,16 +4,22 @@ let
     clients-per-node = 2;
     skip-build = false;
   };
+  accessKeyId = "cbcast";
 in
 {
   network.description = "machines on aws";
 
-  defaults = {
+  resources.ec2KeyPairs.nixops-admin = {
+    inherit accessKeyId;
+    region = "us-west-1";
+  };
+
+  defaults = { resources, ... }: {
     # tell nixops where to deploy
     deployment.targetEnv = "ec2";
     deployment.ec2.instanceType = "t3.micro";
-    deployment.ec2.keyPair = "cbcast";
-    deployment.ec2.privateKey = "./cbcast-test-deploy-2021-group.csv";
+    deployment.ec2.accessKeyId = accessKeyId; # refers to ~/.ec2-keys or ~/.aws/credentials
+    deployment.ec2.keyPair = resources.ec2KeyPairs.nixops-admin;
 
     imports = [ ./common.nix ];
   };
