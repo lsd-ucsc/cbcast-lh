052213cf73c672624c23373ebc8bf1eaf44de9e5
PLR <51248199+plredmond@users.noreply.github.com>
2021-03-18 15:53:27 -0700

    import common.nix correctly

--- deploy/net-aws.nix
+++ deploy/net-aws.nix
@@ -8,12 +8,14 @@ in
 {
   network.description = "machines on aws";
 
-  defaults = (import ./common.nix) // {
+  defaults = {
     # tell nixops where to deploy
     deployment.targetEnv = "ec2";
     deployment.ec2.instanceType = "t3.micro";
     deployment.ec2.keyPair = "cbcast";
     deployment.ec2.privateKey = "./cbcast-test-deploy-2021-group.csv";
+
+    imports = [ ./common.nix ];
   };
 }
   // nodes
--- deploy/net-vm.nix
+++ deploy/net-vm.nix
@@ -9,13 +9,15 @@ in
 {
   network.description = "machines on virtualbox";
 
-  defaults = (import ./common.nix) // {
+  defaults = {
     # tell nixops where to deploy
     deployment.targetEnv = "virtualbox";
     deployment.virtualbox.headless = true;
 
     # virtualbox target doesn't work for alternate ports
     services.openssh.ports = (import <nixpkgs> { }).lib.mkForce [ 22 ];
+
+    imports = [ ./common.nix ];
   };
 }
   // nodes
