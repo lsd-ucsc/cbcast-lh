18bebd8222f734578a8546650fc2ad7743f1d50e
PLR <51248199+plredmond@users.noreply.github.com>
2021-03-18 21:14:33 -0700

    push almost all aws configuration options to cluster.nix

--- deploy/net-aws.nix
+++ deploy/net-aws.nix
@@ -3,23 +3,16 @@ let
     node-regions = [ "us-west-1" "us-west-2" "us-east-1" ];
     clients-per-node = 2;
     skip-build = false;
+    accessKeyId = "cbcast";
   };
-  accessKeyId = "cbcast";
 in
 {
   network.description = "machines on aws";
 
-  resources.ec2KeyPairs.nixops-admin = {
-    inherit accessKeyId;
-    region = "us-west-1";
-  };
-
-  defaults = { resources, ... }: {
+  defaults = {
     # tell nixops where to deploy
     deployment.targetEnv = "ec2";
     deployment.ec2.instanceType = "t3.micro";
-    deployment.ec2.accessKeyId = accessKeyId; # refers to ~/.ec2-keys or ~/.aws/credentials
-    deployment.ec2.keyPair = resources.ec2KeyPairs.nixops-admin;
 
     imports = [ ./common.nix ];
   };
--- deploy/net-vm.nix
+++ deploy/net-vm.nix
@@ -13,7 +13,6 @@ in
     # tell nixops where to deploy
     deployment.targetEnv = "virtualbox";
     deployment.virtualbox.headless = true;
-
     # virtualbox target doesn't work for alternate ports
     services.openssh.ports = (import <nixpkgs> { }).lib.mkForce [ 22 ];
 
