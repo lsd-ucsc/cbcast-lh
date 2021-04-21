9a35bd9a58ce648c92fd7deb9bf708c0eb7951d6
PLR <51248199+plredmond@users.noreply.github.com>
2021-04-21 13:34:47 -0700

    set initial root volume to 8gb instead the default from the AMI (which seems to be 3gb?)

--- deploy/net-aws.nix
+++ deploy/net-aws.nix
@@ -13,6 +13,7 @@ in
     deployment.targetEnv = "ec2";
     #deployment.ec2.accessKeyId = we set this on the individual instances in cluster.nix, because it's necessary for the regional SGs and KPs anyway
     deployment.ec2.instanceType = "t3.micro";
+    deployment.ec2.ebsInitialRootDiskSize = 8;
 
     imports = [ ./common.nix ];
   };
