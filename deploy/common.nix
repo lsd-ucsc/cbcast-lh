e2d6ca3951d17693b1631a0a927fba317670e7a4
PLR <51248199+plredmond@users.noreply.github.com>
2021-04-21 13:47:11 -0700

    turn on nix-gc and clean-tmp-dir

--- deploy/common.nix
+++ deploy/common.nix
@@ -1,6 +1,10 @@
 {
   users.mutableUsers = false;
 
+  boot.cleanTmpDir = true;
+  nix.gc.automatic = true;
+  system.autoUpgrade.enable = false; # would be cool, but not in production
+
   # when users are not mutable, the vm tarket requires a password or pubkey set
   users.users."root".password = "trivial plaintext password which will never be used";
 
