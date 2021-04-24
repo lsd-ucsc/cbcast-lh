5bcd756522c08260fec02a4dfd65cd4a85bb54bf
PLR <51248199+plredmond@users.noreply.github.com>
2021-04-23 21:41:41 -0700

    remove old shellscirpts

--- deploy/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -x
-
-./spit.py 54.183.9.206  7780 --mut > kv0.sh
-./spit.py 35.82.28.205  7780 --mut > kv1.sh
-./spit.py 52.204.233.91 7780       > kv2.sh
-
-chmod +x kv*sh
