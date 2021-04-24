7209e11ecab3f5b8099071d3112f0689283bf358
PLR <51248199+plredmond@users.noreply.github.com>
2021-04-23 23:43:40 -0700

    increase the number of requests from each client; add test coordination script; add log of load

---
+++ deploy/run-aws-test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e -u -o pipefail
+set -x
+
+DEPLOYMENT=aws
+NIX_NETWORK=net-aws.nix
+
+if nixops info -d $DEPLOYMENT > /dev/null; then
+    echo found $DEPLOYMENT deployment
+else
+    nixops create -d $DEPLOYMENT $NIX_NETWORK
+    nixops deploy -d $DEPLOYMENT --force-reboot
+fi
+
+nixops ssh-for-each -d $DEPLOYMENT -- journalctl --rotate
+nixops ssh-for-each -d $DEPLOYMENT -- journalctl --vacuum-size=1B
+nixops ssh-for-each -d $DEPLOYMENT -- nix-collect-garbage
+nixops reboot -d $DEPLOYMENT #nixops ssh-for-each -d $DEPLOYMENT -- systemctl restart kv-store
+
+date -u | tee check.log
+nixops check -d $DEPLOYMENT | tee check.log
+
+nixops ssh-for-each -d $DEPLOYMENT -- systemctl start kv-client.service
+
+while sleep 10; do
+    date -u | tee -a check.log
+    nixops check -d $DEPLOYMENT | tee -a check.log
+done
