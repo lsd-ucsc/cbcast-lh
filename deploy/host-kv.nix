214e6dade3e43c428619d3b53c306785fed720bf
PLR <51248199+plredmond@users.noreply.github.com>
2021-04-23 21:40:21 -0700

    deployment to a docker container

--- deploy/host-kv.nix
+++ deploy/host-kv.nix
@@ -1,6 +1,7 @@
 { kv-store-offset
 , kv-store-port
 , node-prefix
+, kv-store-ip-explicit ? null
 , skip-build ? false
 , modules ? [ ]
 }:
@@ -8,7 +9,7 @@
 { pkgs, lib, nodes, ... }:
 let
   node-names = builtins.filter (lib.hasPrefix node-prefix) (builtins.attrNames nodes);
-  get-ip = config: if config.networking.publicIPv4 == null then config.networking.privateIPv4 else config.networking.publicIPv4;
+  get-ip = config: if kv-store-ip-explicit != null then kv-store-ip-explicit else config.networking.publicIPv4; # when do we use config.networking.publicIPv4?
   node-ipports = map (nn: "${get-ip nodes.${nn}.config}:${toString kv-store-port}") node-names;
   node-hostports = map (nn: "${nn}:${toString kv-store-port}") node-names; # nixops populates the hosts file with hostnames
   kv-store-args = "${toString kv-store-offset} ${builtins.concatStringsSep " " node-ipports}";
