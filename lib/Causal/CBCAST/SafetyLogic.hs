304261a58800da2289cc4072bd5426abb672120c
Lindsey Kuper <lindsey@composition.al>
2021-06-29 13:51:41 -0700

    Here's a version that works with only the distinctAtSenderM2 axiom.

--- lib/Causal/CBCAST/SafetyLogic.hs
+++ lib/Causal/CBCAST/SafetyLogic.hs
@@ -36,22 +36,23 @@ type Deliverable MSG PROCVC
 @-}
 type Deliverable = Fin -> Proof
 
--- | @processOrderAxiom@ says that every message sent by a given process has a
--- unique VC value at the sender position. (This follows from the fact that
--- events on a process have a total order.) This is enough to prove safety in
--- the same-sender case, because we already know that m1 -> m2, so we know that
--- for each position i in their respective VCs, VC(m1)[i] <= VC(m2)[i]. This
--- axiom rules out the case where they're equal, so then we know that VC(m1)[i]
--- < VC(m2)[i], which is the fact that LH needs to complete the proof.
+-- | @distinctAtSenderM2@ says that, given two messages m1 and m2
+-- where m1 -> m2, their VC entries are distinct in the position of
+-- m2's sender.  (This is the case since in any execution that can
+-- occur, if m1 -> m2, then m2 could not have known about m1's send,
+-- so m2 would have, at a minimum, an entry in its own VC position
+-- that is one larger than m1 in the corresponding position.  However,
+-- we cannot prove this in our formalism because we haven't formalized
+-- a notion of what executions can occur.)
 {-@
-assume processOrderAxiom
-    ::    m1 : Message r
-    ->  { m2 : Message r | m1 != m2 }
-    ->  { _:Proof | mSender m1 == mSender m2 }
-    ->  { _:Proof | vcReadK (mSent m1) (mSender m1) != vcReadK (mSent m2) (mSender m2) }
+assume distinctAtSenderM2
+    :: m1 : Message r
+    -> m2 : Message r
+    -> CausallyBefore {m1} {m2}
+    -> { _:Proof | vcReadK (mSent m1) (mSender m2) != vcReadK (mSent m2) (mSender m2) }
 @-}
-processOrderAxiom :: Message r -> Message r -> Proof -> Proof
-processOrderAxiom _m1 _m2 _proof = ()
+distinctAtSenderM2 :: Message r -> Message r -> CausallyBefore -> Proof
+distinctAtSenderM2 _m1 _m2 _m1_before_m2 = ()
 
 {-@ ple safety2 @-}
 {-@
@@ -68,62 +69,12 @@ safety2 p m1 m2 m2_deliverable_p m1_before_m2 k
     | k /= mSender m2 = () ? m1_before_m2 k ? m2_deliverable_p k
     | k == mSender m2 =
         if k == mSender m1
-        then () ? m1_before_m2 k ? m2_deliverable_p k ? processOrderAxiom m1 m2 ()
-        -- Case where k /= mSender m2
+        then () ? m1_before_m2 k ? m2_deliverable_p k ? distinctAtSenderM2 m1 m2 m1_before_m2
+        -- Case where k /= mSender m1
         else () ? vc_m1_k_eq_vc_p_k_plus_1
                    k p m1 m2
-                   (vc_m1_k_lt_vc_m2_k k p m1 m2 m1_before_m2)
+                   (vc_m1_k_lt_vc_m2_k k m1 m2 m1_before_m2)
                    (vc_m2_k_equals_vc_p_k_plus_1 k p m2 m2_deliverable_p) *** QED
-
-
--- Might need a lemma that says:  if mSender m1 /= mSender m2 and m1 -> m2,
--- then m1 must have been delivered at mSender m2.                   
--- Returns something of type Delivered {m1} (mSent m2).
-{-@
-deliveredInBetween
-    :: m1 : Message r
-    -> m2 : Message r
-    -> CausallyBefore {m1} {m2}
-    -> Delivered {m1} {mSent m2}
-@-}
-deliveredInBetween :: Message r -> Message r -> CausallyBefore -> Delivered
-deliveredInBetween m1 m2 m1_before_m2 k = () ? m1_before_m2 k
-
-
--- At some point after this intermediate delivery takes place on
--- sender(m2), m2 is sent.  We know that when m2 is sent, sender(m2)
--- increments its own position first, so the VC must have been
--- strictly smaller in the sender(m2) position at the intermediate
--- delivery time than it is in m2's VC.
-
--- | There's a VC value, call it vc_between, that is the VC of sender(m2)
--- immediately before sender(m2) is incremented for m2's send.  Since
--- m1 had to already have been delivered at sender(m2) at this point,
--- vc_between must be at least as big as VC(m1), but is less than VC(m2) in
--- the sender(m2) position.
-
--- Patrick says try this: vcReadK (vcTick vc_between k) k == vcReadK (mSent m2) k
--- { vc_between : VC | vcTick k vc_between == mSent m2 }
--- { vc_between : VC | vc_between == (vcBackTick k (mSent m2)) }
-{-@ ple vcInBetween @-}
-{-@
-vcInBetween
-    ::    k : PID
-    ->   m1 : Message r
-    -> { m2 : Message r | mSender m2 == k && mSender m1 /= mSender m2 }
-    -> CausallyBefore {m1} {m2}
-    -> { vc_between : VC | vc_between == (vcBackTick k (mSent m2)) }
-    -> { _:Proof | vcReadK (mSent m1) k <= vcReadK vc_between k &&
-                   vcReadK vc_between k <  vcReadK (mSent m2) k }
-@-}
-vcInBetween :: PID -> Message r -> Message r -> CausallyBefore -> VC -> Proof
-vcInBetween k m1 m2 m1_before_m2 vc
-    =   ()  ? m1_before_m2 k
-            ? deliveredInBetween m1 m2 m1_before_m2 k
-    *** Admit
-    -- m1 from _ at 1,1,1
-    -- m2 from 1 at 1,1,2
-    --         back 1,0,2
 
 -- | VC(m1)[k] < VC(m2)[k] since m1 -> m2 and since there is a VC
 -- value that's in between at that position, so they can't be equal at
@@ -132,14 +83,14 @@ vcInBetween k m1 m2 m1_before_m2 vc
 {-@
 vc_m1_k_lt_vc_m2_k
     :: k : PID
-    -> p : VC
     -> m1 : Message r
     -> { m2 : Message r | mSender m2 == k && mSender m1 /= mSender m2 }
     -> CausallyBefore {m1} {m2}
     -> { _:Proof | vcReadK (mSent m1) k < vcReadK (mSent m2) k }
 @-}
-vc_m1_k_lt_vc_m2_k :: PID -> VC -> Message r -> Message r -> CausallyBefore -> Proof
-vc_m1_k_lt_vc_m2_k k _p m1 m2 m1_before_m2 = () ? vcInBetween k m1 m2 m1_before_m2 (vcBackTick (mSender m2) (mSent m2)) ? m1_before_m2 k *** QED
+vc_m1_k_lt_vc_m2_k :: PID -> Message r -> Message r -> CausallyBefore -> Proof
+vc_m1_k_lt_vc_m2_k k m1 m2 m1_before_m2 =
+  () ? m1_before_m2 k ? distinctAtSenderM2 m1 m2 m1_before_m2 *** QED
 
 -- | Since we have deliverable(m2, p), we have VC(m2)[k] = VC(p)[k]+1
 -- for k = sender(m2).
