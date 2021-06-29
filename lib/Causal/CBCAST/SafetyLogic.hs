85e91008b84613e5d6999ad5dc3f129f902d2e8d
Lindsey Kuper <lindsey@composition.al>
2021-06-29 16:19:42 -0700

    Hook up actual defs of deliverable and causallyBefore.

--- lib/Causal/CBCAST/SafetyLogic.hs
+++ lib/Causal/CBCAST/SafetyLogic.hs
@@ -7,43 +7,85 @@ import Redefined
 import Causal.CBCAST.VectorClock
 import Causal.CBCAST.Message
 
+{-@ ple iterImpliesForall @-}
 {-@
-type CausallyBefore M1 M2
-    =   k : PID
-    ->  { _:Proof | ( vcReadK (mSent M1) k <= vcReadK (mSent M2) k )
-                 && (          mSent M1    /=          mSent M2    )
-        }
+iterImpliesForall
+    :: n:Nat
+    -> p:(Fin {n} -> Bool)
+    -> { _:Proof | iter n p }
+    -> (k:Fin {n} -> { _:Proof | p k })
 @-}
-type CausallyBefore = Fin -> Proof
--- TODO: write in terms of VCLess rather than rewriting its definition here
+iterImpliesForall :: Int -> (Fin -> Bool) -> Proof -> (Fin -> Proof)
+iterImpliesForall n p satisfied k
+    -- Insight: boolAnd (p (n - 1)) (listFoldr boolAnd True (listMap p (fin (n - 1))))
+    | k == n - 1 = ()
+    | k <  n - 1 = iterImpliesForall (n - 1) p satisfied k
+
+{-@ reflect causallyBeforeK @-}
+{-@
+causallyBeforeK :: Message r -> Message r -> PID -> Bool @-}
+causallyBeforeK :: Message r -> Message r -> PID -> Bool
+causallyBeforeK m1 m2 k = vcLessK (mSent m1) (mSent m2) k
+
+{-@ reflect causallyBefore @-}
+{-@
+causallyBefore :: Message r -> Message r -> Bool @-}
+causallyBefore :: Message r -> Message r -> Bool
+causallyBefore m1 m2 = vcLess (mSent m1) (mSent m2)
+
+{-@
+type CausallyBefore M1 M2 = k:PID -> { _:Proof | causallyBeforeK M1 M2 k }
+@-}
+type CausallyBefore = PID -> Proof
+
+{-@ ple cb_implies_CB @-}
+{-@
+cb_implies_CB
+    ::  m1 : Message r
+    ->  m2 : Message r
+    ->  { _:Proof | causallyBefore m1 m2 }
+    ->  CausallyBefore m1 m2
+@-}
+cb_implies_CB :: Message r -> Message r -> Proof -> CausallyBefore
+cb_implies_CB m1 m2 causallyBeforeSatisfied pid =
+    iterImpliesForall (vcSize (mSent m1)) p causallyBeforeSatisfied pid
+  where
+    p = vcLessK (mSent m1) (mSent m2)
 
 {-@
 type Delivered MSG PROCVC
     =   k : PID
-    ->  { _:Proof | vcReadK (mSent MSG) k <= vcReadK PROCVC k
-        }
+    ->  { _:Proof | vcReadK (mSent MSG) k <= vcReadK PROCVC k }
 @-}
-type Delivered = Fin -> Proof
+type Delivered = PID -> Proof
 
 {-@
-type Deliverable MSG PROCVC
-    =   k : PID
-    ->  { _:Proof | ( k == mSender MSG => vcReadK (mSent MSG) k == vcReadK PROCVC k + 1 )
-                 && ( k /= mSender MSG => vcReadK (mSent MSG) k <= vcReadK PROCVC k     )
-        }
+type Deliverable M P = k:PID -> { _:Proof | deliverableK M P k }
 @-}
-type Deliverable = Fin -> Proof
+type Deliverable = PID -> Proof
+
+{-@ ple d_implies_D @-}
+{-@
+d_implies_D
+    ::  procVc: VC
+    ->  m : Message r
+    ->  { _:Proof | deliverable m procVc }
+    ->  Deliverable m procVc
+@-}
+d_implies_D :: VC -> Message r -> Proof -> Deliverable
+d_implies_D procVc m deliverableSatisfied pid =
+    iterImpliesForall (vcSize (mSent m)) (deliverableK m procVc) deliverableSatisfied pid
 
 -- | @distinctAtSenderM2@ says that, given two messages m1 and m2
 -- where m1 -> m2, their VC entries are distinct in the position of
 -- m2's sender.  (This is the case since in any execution that can
--- occur, if m1 -> m2, then m2's sender knows about m1's send, but
--- m1's sender cannot have known about m2's send.  So m2 will have a
--- VC with an entry in its own sender's position that is, at a
--- minimum, one larger than m1 in the corresponding position, to
--- account for its own send.  However, we cannot prove this in our
--- formalism because we haven't formalized a notion of what executions
--- can occur.)
+-- occur, if m1 -> m2, then m2's sender knows about m2's send at the
+-- time it sends m2, but m1's sender cannot have known about m2's send
+-- at the time it sends m1.  So m2 will have a VC with an entry in its
+-- own sender's position that is, at a minimum, one larger than m1 in
+-- the corresponding position, to account for its own send of m2.
+-- However, we cannot prove this in our formalism because we haven't
+-- formalized a notion of what executions can occur.)
 {-@
 assume distinctAtSenderM2
     :: m1 : Message r
@@ -60,21 +102,21 @@ safety2
     :: p : VC
     -> m1 : Message r
     -> m2 : Message r
-    -> Deliverable {m2} {p}
-    -> CausallyBefore {m1} {m2}
+    -> { _:Proof | deliverable m2 p }
+    -> { _:Proof | causallyBefore m1 m2 }
     -> Delivered {m1} {p}
 @-}
-safety2 :: VC -> Message r -> Message r -> Deliverable -> CausallyBefore -> Delivered
+safety2 :: VC -> Message r -> Message r -> Proof -> Proof -> Delivered
 safety2 p m1 m2 m2_deliverable_p m1_before_m2 k
-    | k /= mSender m2 = () ? m1_before_m2 k ? m2_deliverable_p k
+    | k /= mSender m2 = () ? (cb_implies_CB m1 m2 m1_before_m2) k ? (d_implies_D p m2 m2_deliverable_p) k
     | k == mSender m2 =
         if k == mSender m1
-        then () ? m1_before_m2 k ? m2_deliverable_p k ? distinctAtSenderM2 m1 m2 m1_before_m2
+        then () ? (cb_implies_CB m1 m2 m1_before_m2) k ? (d_implies_D p m2 m2_deliverable_p) k ? distinctAtSenderM2 m1 m2 (cb_implies_CB m1 m2 m1_before_m2)
         -- Case where k /= mSender m1
         else () ? vc_m1_k_eq_vc_p_k_plus_1
                    k p m1 m2
-                   (vc_m1_k_lt_vc_m2_k k m1 m2 m1_before_m2)
-                   (vc_m2_k_equals_vc_p_k_plus_1 k p m2 m2_deliverable_p) *** QED
+                   (vc_m1_k_lt_vc_m2_k k m1 m2 (cb_implies_CB m1 m2 m1_before_m2))
+                   (vc_m2_k_equals_vc_p_k_plus_1 k p m2 (d_implies_D p m2 m2_deliverable_p)) *** QED
 
 -- | VC(m1)[k] < VC(m2)[k] since m1 -> m2, so all entries are <=, and
 -- since they have to be distinct at m2's sender's position, which is
