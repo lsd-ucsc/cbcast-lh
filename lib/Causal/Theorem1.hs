4afd69a94c793a551c008ac3d0fe10f241ba2c1e
PLR <51248199+plredmond@users.noreply.github.com>
2021-09-15 01:00:39 -0700

    write theorem 1

--- lib/Causal/Theorem1.hs
+++ lib/Causal/Theorem1.hs
@@ -1,15 +1,15 @@
+{-# OPTIONS_GHC "-Wno-unused-imports" #-} -- LH needs the qualified imports for SMT things
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 module Causal.Theorem1 where
 
 import Language.Haskell.Liquid.ProofCombinators
 
-import Data.Assoc
-import Data.BinaryRelation
+import qualified Data.Assoc
+import qualified Data.BinaryRelation
 
-import Redefined.Tuple
 import Redefined.List
-import Redefined.Set
+import qualified Redefined.Set
 
 import Causal.Execution.Type
 import Causal.Execution.Semantics
@@ -22,39 +22,47 @@ import Causal.Execution.Reachable
 type DeliverablePredicate p m = m -> ProcessState p m -> Bool
 
 
--- ** Causally Safe
-
--- | Has the message been delivered? (Is the message contained in a delivery
--- event in the process state?)
-{-@ reflect delivered @-}
-delivered :: Eq m => m -> ProcessState p m -> Bool
-delivered m s = listOrMap (eventDeliversMessage m) s
-
--- | Is the event a delivery of the message?
-{-@ reflect eventDeliversMessage @-}
-eventDeliversMessage :: Eq m => m -> Event p m -> Bool
-eventDeliversMessage message (Deliver _p m) = message == m
-eventDeliversMessage _message (Broadcast _m) = False
+-- ** Causal safety property
 
 -- | Property of a 'DeliverablePredicate' @D@ given some happens-before
--- relation @HB@.
+-- relation @HB@, for all messages and process states meeting the
+-- preconditions.
 {-@
 type CausallySafeProp p m HB D
     =    m1 : m
     -> { m2 : m | HB (Broadcast m1) (Broadcast m2) }
     -> { s : ProcessState p m | D m2 s }
-    -> { delivered m1 s }
+    -> { stateDelivered s m1 }
 @-}
 type CausallySafeProp p m = m -> m -> ProcessState p m -> Proof
 
 
 -- ** Guarded by Deliverable
 
+
+-- *** Guarded by as a property
+
+-- | Property of an 'Execution' @X@ and a 'DeliverablePredicate' @D@, for all
+-- messages, processes, and process states meeting the preconditions.
+{-@
+type GuardedByProp p m X D
+    =    m : m
+    ->   p : p
+    -> { s : ProcessState p m | xProcessHasStatePriorToEvent X p (Deliver p m) s }
+    -> { D m s }
+@-}
+type GuardedByProp p m = m -> p -> ProcessState p m -> Proof
+
+
+-- *** Guarded by via foldr
+
+{-@ reflect guardedByImpl @-}
 {-@ guardedByImpl :: ValidRules p m -> DeliverablePredicate p m -> Maybe (Execution p m) @-}
 guardedByImpl :: (Ord p, Ord m) => [Rule p m] -> DeliverablePredicate p m -> Maybe (Execution p m)
 guardedByImpl rules deliverablePredicate =
     listFoldr (guardedByImplHelper deliverablePredicate) (Just execution0) rules
 
+{-@ reflect guardedByImplHelper @-}
 guardedByImplHelper :: (Ord p, Ord m) => DeliverablePredicate p m -> Rule p m -> Maybe (Execution p m) -> Maybe (Execution p m)
 guardedByImplHelper _deliverablePredicate _rule Nothing = Nothing
 guardedByImplHelper _deliverablePredicate rule@BroadcastRule{} acc@Just{} = applyRulesHelper rule acc
@@ -63,3 +71,49 @@ guardedByImplHelper deliverablePredicate rule@(DeliverRule process message) acc@
     then applyRulesHelper rule acc
     else Nothing
 
+-- | Rules which produce an execution because /in addition to the premises
+-- holding at each step/ a deliverable predicate @D@ holds at each step.
+{-@
+type RulesGuardedBy p m D =
+    { rules : ValidRules p m | isJust (guardedByImpl rules D) }
+@-}
+
+
+-- ** Test out the props
+
+-- | Dummy deliverable predicate used for testing the LH type aliases.
+{-@ reflect exConstDP @-}
+exConstDP :: DeliverablePredicate p m
+exConstDP _m _s = True
+
+{-@ exCausallySafeConst :: CausallySafeProp p m {xHappensBefore execution0} {exConstDP} @-}
+exCausallySafeConst :: CausallySafeProp p m
+exCausallySafeConst _m1 _m2 _s = () *** Admit
+
+{-@ ple exRulesGuardedByConst @-}
+{-@ exRulesGuardedByConst :: RulesGuardedBy p m {exConstDP} @-}
+exRulesGuardedByConst :: [Rule p m]
+exRulesGuardedByConst = []
+
+{-@ exGuardedByProp :: GuardedByProp p m {execution0} {exConstDP} @-}
+exGuardedByProp :: GuardedByProp p m
+exGuardedByProp _m _p _s = () *** Admit
+
+
+-- ** Theorem 1 proof
+
+{-@
+theorem1
+    ::   d : DeliverablePredicate p m
+    ->  vr : ValidRules p m
+    -> CausallySafeProp p m {xHappensBefore (applyValidRules vr)} {d}
+    -> GuardedByProp p m {applyValidRules vr} {d}
+    -> CausalDeliveryProp p m {applyValidRules vr}
+@-}
+theorem1
+    :: DeliverablePredicate p m
+    -> [Rule p m]
+    -> CausallySafeProp p m
+    -> GuardedByProp p m
+    -> CausalDeliveryProp p m
+theorem1 _d _vr _csP _gbP _p _s _m1 _m2 = () *** Admit -- TODO
