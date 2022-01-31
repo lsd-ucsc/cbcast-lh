a5fa994e261ccc25105218807cba24049872be76
PLR <51248199+plredmond@users.noreply.github.com>
2022-01-30 19:39:11 -0800

    prove irrefl,antisym -> asym

--- lib/Properties.hs
+++ lib/Properties.hs
@@ -4,26 +4,48 @@
 {-# LANGUAGE NamedFieldPuns #-}
 module Properties where
 
----- {-@ LIQUID "--check-var=vcLessReflexive" @-}
 
 import Language.Haskell.Liquid.ProofCombinators
 import SystemModel
 
-{-@ type   Reflexive a OP = x:a -> {OP x x} @-}
-{-@ type Irreflexive a OP = x:a -> {not (OP x x)} @-}
+{-@ type   Reflexive a R = x:a -> {R x x} @-}
+{-@ type Irreflexive a R = x:a -> {not (R x x)} @-}
 
-{-@ type     Symmetric a OP = x:a -> {y:a | OP x y} -> {OP y x} @-}
-{-@ type    Asymmetric a OP = x:a -> {y:a | OP x y} -> {not (OP y x)} @-}
-{-@ type Antisymmetric a OP = x:a -> {y:a | OP x y && OP y x} -> {x == y} @-}
+{-@ type     Symmetric a R = x:a -> {y:a | R x y} -> {R y x} @-}
+{-@ type    Asymmetric a R = x:a -> {y:a | R x y} -> {not (R y x)} @-}
+{-@ type Antisymmetric a R = x:a -> {y:a | R x y && R y x} -> {x == y} @-}
 
-{-@ type Transitive a OP = x:a -> {y:a | OP x y} -> {z:a | OP y z} -> {OP x z} @-}
-{-@ type Total a OP = x:a -> y:a -> {(OP x y || OP y x) && not (OP x y && OP y x)} @-}
+{-@ type Transitive a R = x:a -> {y:a | R x y} -> {z:a | R y z} -> {R x z} @-}
+{-@ type Total a R = x:a -> y:a -> {(R x y || R y x) && not (R x y && R y x)} @-}
 
 -- Preorder                  : Transitive, Reflexive
 -- Partial order (non-strict): Transitive, Reflexive, Antisymmetric
 -- Total order               : Transitive, Reflexive, Antisymmetric, Total
 -- Partial order (strict)    : Transitive, (ONE-OF: Irreflexive, Asymmetric)
 -- Equivalence               : Transitive, Reflexive, Symmetric
+
+
+
+
+-- * Generic properties
+
+-- | Irreflexive and Antisymmetric imply Asymmetric.
+--
+--   ( ∀ x. ¬xRx )
+-- ∧ ( ∀ x y. xRy ∧ yRx → x≡y )
+-- ⇒
+--   ( ∀ x y. xRy → ¬yRx )
+{-@
+irreflexiveAntisymmetric :: r:_ -> Irreflexive a {r} -> Antisymmetric a {r} -> Asymmetric a {r} @-}
+irreflexiveAntisymmetric :: Eq a => (a -> a -> Bool) -> (a -> Proof) -> (a -> a -> Proof) -> (a -> a -> Proof)
+irreflexiveAntisymmetric r irrefl antisym x y
+    | r x y && r y x
+        =   x ? antisym x y
+        === y ? irrefl y
+        --- `x == y && not (r y y)` contradicts the case assumption
+        *** QED
+    | otherwise
+        = () -- assumed the conclusion `not (r a b)` or the reverse
 
 
 
