2c4c654cb165c32ae5c4f02da376294b01ba81ac
PLR <51248199+plredmond@users.noreply.github.com>
2022-02-09 11:43:07 -0800

    lemmas for delivery CHA pres -- unfinished

--- lib/UCausalDeliveryPLCD.hs
+++ lib/UCausalDeliveryPLCD.hs
@@ -191,6 +191,15 @@ deliverVcIsPrevCombineMsg :: {p1:P r | isJust (deliver p1)}
     -> {vcCombine (pVC p1) (mVC m) == pVC p2}
 @-}
 deliverVcIsPrevCombineMsg :: P r -> M r -> P r -> Proof
+deliverVcIsPrevCombineMsg p₁ m p₂ = --- by cases of deliver
+    let Just (m', pDQ') = dequeue (pVC p₁) (pDQ p₁) in -- one case, due to premise
+        p₂ -- restate part of conclusion
+        ? (m === m')
+    === p₁  { pVC = vcCombine (pVC p₁) (mVC m)
+            , pDQ = pDQ'
+            , pHist = Deliver (pID p₁) (coerce m) : pHist p₁
+            } -- by def of deliver
+    *** QED
 
 {-@ ple deliverHistVcIsPrevCombineMsg @-}
 {-@
@@ -200,18 +209,33 @@ deliverHistVcIsPrevCombineMsg :: {p1:P r | isJust (deliver p1)}
     -> {vcCombine (pHistVC p1) (mVC m) == pHistVC p2}
 @-}
 deliverHistVcIsPrevCombineMsg :: P r -> M r -> P r -> Proof
+deliverHistVcIsPrevCombineMsg p₁ m p₂
+    =   pHistVC p₂
+        ? recordUpdateLemma
+    === pHistVCHelper n (e : pHist p₁) -- by def of pHistVC
+    === vcCombine (eventVC n e) (pHistVCHelper n (pHist p₁)) -- by def of pHistVCHelper
+  where
+    n = listLength (pVC p₁)
+    e = Deliver (pID p₁) (coerce m)
+    -- by cases of deliver
+    Just (m', pDQ') = dequeue (pVC p₁) (pDQ p₁) -- one case, due to premise
+    recordUpdateLemma
+        =   p₂  ? (m === m') -- restate part of conclusion
+        === p₁  { pVC = vcCombine (pVC p₁) (mVC m)
+                , pDQ = pDQ'
+                , pHist = e : pHist p₁
+                } -- by def of deliver
 
 {-@ ple deliverCHApres @-}
 {-@
 deliverCHApres :: n:Nat -> CHApreservation r {n} {deliverShim} @-}
 deliverCHApres :: Int -> P r -> Proof -> Proof
-deliverCHApres n p _pCHA =
-    -- cases from deliver
+deliverCHApres n p _pCHA = -- by cases of deliver
     case dequeue (pVC p) (pDQ p) of
         Nothing -> () -- p is unchanged
-        Just (m, pDQ')
-            ->  let p' = deliverShim p
-                    e = Deliver (pID p) (coerce m)
+        Just (m, pDQ') ->
+            let p' = deliverShim p
+                e = Deliver (pID p) (coerce m)
             in  vcLessEqual (pHistVC p') (pVC p') -- restate conclusion
                 ? deliverVcIsPrevCombineMsg p m p'
                 ? deliverHistVcIsPrevCombineMsg p m p'
