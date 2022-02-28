16bfe8300febe9b3ba9cb7aac4c75c8cb669f630
PLR <51248199+plredmond@users.noreply.github.com>
2022-02-27 22:01:05 -0800

    rewrite pHistVC to be slightly simpler

--- lib/UCausalDelivery_CHA.hs
+++ lib/UCausalDelivery_CHA.hs
@@ -28,9 +28,10 @@ type CHApreservation r N OP
 {-@
 pEmptyCHA :: n:Nat -> p_id:PIDsized {n} -> ClockHistoryAgreement {pEmpty n p_id} @-}
 pEmptyCHA :: Int -> Fin -> Proof
-pEmptyCHA n p_id
-    =   let p = pEmpty n p_id
-    in  vcLessEqual (pHistVC p) (pVC p) -- restate conclusion
+pEmptyCHA n p_id =
+    let p = pEmpty n p_id in
+        pHistVC p `vcLessEqual` pVC p -- restate conclusion
+    === vcEmpty n `vcLessEqual` vcEmpty n -- by def of pEmpty,pHistVC,pHistVCHelper
         ? vcLessEqualReflexive (vcEmpty n)
     *** QED
 
@@ -40,39 +41,23 @@ pEmptyCHA n p_id
 -- * Hist VC
 
 {-@
-eventN :: Event VCMM r -> Nat @-}
-eventN :: Event VCMM r -> Int
-eventN (Broadcast m) = listLength (mVC m)
-eventN (Deliver _pid m) = listLength (mVC m)
-{-@ inline eventN @-} -- Required so that eventVC's annotation passes.
-
--- | The vc for the message in an event.
-{-@
-eventVC :: e:Event VCMM r -> VCsized {eventN e} @-}
-eventVC :: Event VCMM r -> VC
-eventVC (Broadcast m) = mVC m
-eventVC (Deliver _pid m) = mVC m
-{-@ reflect eventVC @-}
-
--- | The supremum of vector clocks on events in a process history.
-{-@
-pHistVC :: p:P r -> VCasP {p} @-}
-pHistVC :: P r -> VC
-pHistVC p = pHistVCHelper (listLength (pVC p)) (pHist p)
-{-@ reflect pHistVC @-}
+eventMessage :: n:Nat -> Event (VCMMsized {n}) r -> Msized r {n} @-}
+eventMessage :: Int -> Event VCMM r -> M r
+eventMessage _n (Broadcast (Message a b)) = Message a b
+eventMessage _n (Deliver _pid (Message a b)) = Message a b
+{-@ reflect eventMessage @-}
 
 {-@
 pHistVCHelper :: n:Nat -> Hsized r {n} -> VCsized {n} @-}
 pHistVCHelper :: Int -> H r -> VC
 pHistVCHelper n [] = vcEmpty n
 pHistVCHelper n (Broadcast{}:es) = pHistVCHelper n es
-pHistVCHelper n (e@Deliver{}:es) = (eventVC e `proofConst` vcmmSizedEventVC n e) `vcCombine` pHistVCHelper n es
+pHistVCHelper n (e@Deliver{}:es) = mVC (eventMessage n e) `vcCombine` pHistVCHelper n es
 {-@ reflect pHistVCHelper @-}
 
-{-@ ple vcmmSizedEventVC @-}
+-- | The supremum of vector clocks on delivered messages in a process history.
 {-@
-vcmmSizedEventVC :: n:Nat -> e:Event (VCMMsized {n}) r -> { n == len (eventVC e) } @-}
-vcmmSizedEventVC :: Int -> Event VCMM r -> Proof
-vcmmSizedEventVC _n (Broadcast    m) = mVC m === vcmmSent (mMetadata m) *** QED
-vcmmSizedEventVC _n (Deliver _pid m) = mVC m === vcmmSent (mMetadata m) *** QED
-
+pHistVC :: p:P r -> VCasP {p} @-}
+pHistVC :: P r -> VC
+pHistVC p = pHistVCHelper (listLength (pVC p)) (pHist p)
+{-@ reflect pHistVC @-}
