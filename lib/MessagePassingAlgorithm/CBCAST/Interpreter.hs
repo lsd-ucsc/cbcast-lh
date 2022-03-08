93cc24fa4fe733a5536a715942d71d41a458b773
PLR <51248199+plredmond@users.noreply.github.com>
2022-03-07 23:43:48 -0800

    constrain output sizes to match inputs sizes

--- lib/MessagePassingAlgorithm/CBCAST/Interpreter.hs
+++ lib/MessagePassingAlgorithm/CBCAST/Interpreter.hs
@@ -9,25 +9,35 @@ import MessagePassingAlgorithm.VectorClockAdapter
 import MessagePassingAlgorithm.CBCAST
 
 data Input r
-    = InputReceive (M r) (P r)
-    | InputDeliver (P r)
-    | InputBroadcast r (P r)
+    = InputReceive Int (M r) (P r)
+    | InputDeliver Int (P r)
+    | InputBroadcast Int r (P r)
 {-@
 data Input r
     = InputReceive
-        { inputReceiveMessage::M r
-        , inputReceiveProcess::PasM r {inputReceiveMessage}
+        { inputReceiveN::Nat
+        , inputReceiveMessage::Msized r {inputReceiveN}
+        , inputReceiveProcess::Psized r {inputReceiveN}
         }
     | InputDeliver
-        { inputDeliverProcess::P r
+        { inputDeliverN::Nat
+        , inputDeliverProcess::Psized r {inputDeliverN}
         }
     | InputBroadcast
-        { inputBroadcastRaw::r
-        , inputBroadcastProcess::P r
+        { inputBroadcastN::Nat
+        , inputBroadcastRaw::r
+        , inputBroadcastProcess::Psized r {inputBroadcastN}
         }
 @-}
 
--- What about output sizes?
+{-@
+inputSize :: Input r -> Nat @-}
+inputSize :: Input r -> Int
+inputSize (InputReceive n _ _)   = n
+inputSize (InputDeliver n _)     = n
+inputSize (InputBroadcast n _ _) = n
+{-@ measure inputSize @-}
+
 data Output r
     = OutputReceive Int (P r)
     | OutputDeliver Int (Maybe (M r, P r))
@@ -49,14 +59,20 @@ data Output r
 @-}
 
 {-@
-pSize :: P r -> Nat @-}
-pSize :: P r -> Int
-pSize p = listLength (pVC p)
-{-@ inline pSize @-}
+outputSize :: Output r -> Nat @-}
+outputSize :: Output r -> Int
+outputSize (OutputReceive n _)   = n
+outputSize (OutputDeliver n _)   = n
+outputSize (OutputBroadcast n _) = n
+{-@ measure outputSize @-}
 
--- {-@ ple interpret @-}
-interpret :: Input r -> Output r
-interpret (InputReceive m p)    = OutputReceive (pSize p) (internalReceive m p)
-interpret (InputDeliver p)      = OutputDeliver (pSize p) (internalDeliver p)
-interpret (InputBroadcast r p)  = OutputBroadcast (pSize p) (internalBroadcast r p)
-{-@ reflect interpret @-}
+{-@
+step
+    ::   i:Input r
+    -> { o:Output r | inputSize i == outputSize o }
+@-}
+step :: Input r -> Output r
+step (InputReceive n m p)   = OutputReceive n (internalReceive m p)
+step (InputDeliver n p)     = OutputDeliver n (internalDeliver p)
+step (InputBroadcast n r p) = OutputBroadcast n (internalBroadcast r p)
+{-@ reflect step @-}
