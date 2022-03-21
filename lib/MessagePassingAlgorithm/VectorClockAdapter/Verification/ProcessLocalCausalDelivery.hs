11a67fd22b15a85c5ae748b998ce96c36981f689
PLR <51248199+plredmond@users.noreply.github.com>
2022-03-21 16:17:01 -0700

    go back to only one processOrder function

--- lib/MessagePassingAlgorithm/VectorClockAdapter/Verification/ProcessLocalCausalDelivery.hs
+++ lib/MessagePassingAlgorithm/VectorClockAdapter/Verification/ProcessLocalCausalDelivery.hs
@@ -18,7 +18,7 @@ type ProcessLocalCausalDelivery r PID PHIST
     =  {m1 : M r         | listElem (Deliver PID m1) PHIST }
     -> {m2 : MasM r {m1} | listElem (Deliver PID m2) PHIST
                 && vcLess (mVC m1) (mVC m2) }
-    -> {_ : Proof | processOrder2 PHIST (Deliver PID m1) (Deliver PID m2) }
+    -> {_ : Proof | processOrder PHIST (Deliver PID m1) (Deliver PID m2) }
 @-}
 
 -- | The empty MPA process history observes process local causal delivery. This
@@ -35,24 +35,20 @@ emptyPHistObservesPLCD _p _m1 _m2 = ()
 
 -- * Process order
 
--- | Process order defined without the constraint on events being unique. This
--- effectively axiomatizes our belief that the list of events contains unique
--- events, which we will prove in future work.
-processOrder :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Event mm r -> Bool
-processOrder hist e e' = listElem e (listTailForHead e' hist)
-{-@ reflect processOrder @-}
-
 {-@ type UniqueProcessHistory mm r = ProcessHistory<{\x y -> x /= y}> mm r @-}
 
 -- | Process order e→e' indicates that e appears in the subsequence of process
 -- history prior to e'.
 --
 -- ∀e,e',xs. e→e' ⇔ e ∈ …:e':xs
-{-@
-processOrder2 :: UniqueProcessHistory mm r -> _ -> _ -> _ @-}
-processOrder2 :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Event mm r -> Bool
-processOrder2 hist e e' = listElem e (listTailForHead e' hist)
-{-@ reflect processOrder2 @-}
+--
+-- Below we prove that when given a history containing distinct events,
+-- processOrder is a strict total order. We define processOrder here without
+-- such a constraint. This effectively axiomatizes our belief that the list of
+-- events contains only unique events, which we will prove in future work.
+processOrder :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Event mm r -> Bool
+processOrder hist e e' = listElem e (listTailForHead e' hist)
+{-@ reflect processOrder @-}
 
 
 
@@ -67,16 +63,16 @@ processOrder2 hist e e' = listElem e (listTailForHead e' hist)
 extendProcessOrder
     ::    h:UniqueProcessHistory mm r
     ->   e1:_
-    -> { e2:_ | processOrder2 h e1 e2 }
+    -> { e2:_ | processOrder h e1 e2 }
     -> { e3:_ | not (listElem e3 h) }
-    -> { processOrder2 (cons e3 h) e1 e2 }
+    -> { processOrder (cons e3 h) e1 e2 }
 @-}
 extendProcessOrder :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Event mm r -> Event mm r -> Proof
 extendProcessOrder h e₁ e₂ e₃
-    {-restate premise-}                 =   processOrder2 h e₁ e₂
-    {-by def of processOrder2-}          === listElem e₁ (listTailForHead e₂ h)
+    {-restate premise-}                 =   processOrder h e₁ e₂
+    {-by def of processOrder-}          === listElem e₁ (listTailForHead e₂ h)
     ? extendElemTail4Head e₁ e₂ h e₃    === listElem e₁ (listTailForHead e₂ (e₃ `uCons` h))
-    {-restate conclusion-}              === processOrder2 (e₃ `uCons` h) e₁ e₂
+    {-restate conclusion-}              === processOrder (e₃ `uCons` h) e₁ e₂
                                         *** QED
 
 
@@ -85,32 +81,32 @@ extendProcessOrder h e₁ e₂ e₃
 -- * procesOrder strict total order
 
 {-@
-processOrderIrreflexiveNoPLE :: hs:UniqueProcessHistory mm r -> Irreflexive (Event mm r) {processOrder2 hs} @-}
+processOrderIrreflexiveNoPLE :: hs:UniqueProcessHistory mm r -> Irreflexive (Event mm r) {processOrder hs} @-}
 processOrderIrreflexiveNoPLE :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Proof
 processOrderIrreflexiveNoPLE [] e =
-    {-restate part of conclusion-}      processOrder2 [] e e
-    {-by def of processOrder2-}      === listElem e (listTailForHead e [])
+    {-restate part of conclusion-}      processOrder [] e e
+    {-by def of processOrder-}      === listElem e (listTailForHead e [])
     {-by def of listTailForHead-}   === listElem e []
     {-by def of listElem-}          === False
                                     *** QED
 processOrderIrreflexiveNoPLE (h:hs) e
   | e == h =
-    {-restate part of conclusion-}      processOrder2 (h:hs) e e
-    {-by def of processOrder2-}      === listElem e (listTailForHead e (h:hs))
+    {-restate part of conclusion-}      processOrder (h:hs) e e
+    {-by def of processOrder-}      === listElem e (listTailForHead e (h:hs))
     {-by def of listTailForHead-}   === listElem e hs
     ? uniqueListHeadNotInTail h hs  === False
                                     *** QED
   | e /= h =
-    {-restate part of conclusion-}              processOrder2 (h:hs) e e
-    {-by def of processOrder2-}              === listElem e (listTailForHead e (h:hs))
+    {-restate part of conclusion-}              processOrder (h:hs) e e
+    {-by def of processOrder-}              === listElem e (listTailForHead e (h:hs))
     {-by def of listTailForHead-}           === listElem e (listTailForHead e hs)
-    {-by def of processOrder2-}              === processOrder2 hs e e
+    {-by def of processOrder-}              === processOrder hs e e
     ? processOrderIrreflexiveNoPLE hs e     === False
                                             *** QED
 
 {-@ ple processOrderIrreflexive @-}
 {-@
-processOrderIrreflexive :: hs:UniqueProcessHistory mm r -> Irreflexive (Event mm r) {processOrder2 hs} @-}
+processOrderIrreflexive :: hs:UniqueProcessHistory mm r -> Irreflexive (Event mm r) {processOrder hs} @-}
 processOrderIrreflexive :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Proof
 processOrderIrreflexive [] _e = () -- trivially ¬(e∈[]) holds
 processOrderIrreflexive (h:hs) e
@@ -119,7 +115,7 @@ processOrderIrreflexive (h:hs) e
 
 {-@ ple processOrderTransitive @-}
 {-@
-processOrderTransitive :: hs:UniqueProcessHistory mm r -> Transitive (Event mm r) {processOrder2 hs} @-}
+processOrderTransitive :: hs:UniqueProcessHistory mm r -> Transitive (Event mm r) {processOrder hs} @-}
 processOrderTransitive :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Event mm r -> Event mm r -> Proof
 processOrderTransitive [] _e₁ e₂ _e₃ = impossible $ listElem e₂ [] -- empty history contradicts premise e₂→e₃
 processOrderTransitive (h:hs) e₁ e₂ e₃
@@ -130,10 +126,10 @@ processOrderTransitive (h:hs) e₁ e₂ e₃
 
 {-@ ple processOrderConnected @-}
 {-@
-processOrderConnected :: hs:UniqueProcessHistory mm r -> Connected ({e:Event mm r | listElem e hs}) {processOrder2 hs} @-}
+processOrderConnected :: hs:UniqueProcessHistory mm r -> Connected ({e:Event mm r | listElem e hs}) {processOrder hs} @-}
 processOrderConnected :: (Eq mm, Eq r) => ProcessHistory mm r -> Event mm r -> Event mm r -> Proof
 processOrderConnected [] _e₁ e₂ = impossible $ listElem e₂ [] -- empty history contradicts premise e₁∈hist
 processOrderConnected (h:hs) e₁ e₂
-  | e₁ == h && e₂ /= h = processOrder2 (h:hs) e₂ e₁ ? tailElem e₂ h hs *** QED
-  | e₁ /= h && e₂ == h = processOrder2 (h:hs) e₁ e₂ ? tailElem e₁ h hs *** QED
+  | e₁ == h && e₂ /= h = processOrder (h:hs) e₂ e₁ ? tailElem e₂ h hs *** QED
+  | e₁ /= h && e₂ == h = processOrder (h:hs) e₁ e₂ ? tailElem e₁ h hs *** QED
   | e₁ /= h && e₂ /= h = processOrderConnected hs e₁ e₂ -- neither element is at the front of history so use the inductive assumption
