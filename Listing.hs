30d00994ca10889186bbf243a9d815b99068d1b4
PLR <51248199+plredmond@users.noreply.github.com>
2022-04-07 13:13:14 -0700

    update the listing example

--- Listing.hs
+++ Listing.hs
@@ -1,11 +1,31 @@
+{-# OPTIONS_GHC "-Wno-missing-signatures" #-}
+{-# OPTIONS_GHC "-Wno-orphans" #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
+import Data.Tuple
 import Data.IORef
 
-import Causal.CBCAST.Message
-import Causal.CBCAST.Process
-import Causal.CBCAST.Protocol
+import MessagePassingAlgorithm
+import MessagePassingAlgorithm.VectorClockAdapter
+import MessagePassingAlgorithm.CBCAST
 
--- | Extended example from Fig 4 of the paper (the corrected Alice/Bob/Carol
--- executions).
+broadcast = internalBroadcast
+receive = internalReceive
+deliver = internalDeliver
+
+deriving instance (Show mm, Show r) => Show (Message mm r)
+deriving instance (Show mm, Show r) => Show (Event mm r)
+deriving instance Show VCMM
+deriving instance Show r => Show (P r)
+
+modifyIORefMaybe :: IORef a -> (a -> Maybe (a, b)) -> IO (Maybe b)
+modifyIORefMaybe ref f =
+    atomicModifyIORef ref $
+    \a -> case f a of Nothing      -> (a, Nothing)
+                      Just (a', b) -> (a', Just b)
+
+
+-- | Example figures in the paper (the corrected Alice/Bob/Carol executions).
 main :: IO ()
 main = do
     leftExample
@@ -16,74 +36,72 @@ main = do
 leftExample :: IO ()
 leftExample = do
     -- Three causal broadcast processes which send String messages.
-    alice <- newIORef (pNew 0 3 :: Process String)
-    carol <- newIORef (pNew 2 3 :: Process String)
+    let n = 3
+    alice <- newIORef (pEmpty n 0 :: P String)
+    bob   <- newIORef (pEmpty n 1 :: P String)
+    carol <- newIORef (pEmpty n 2 :: P String)
 
     -- Alice sends 'lost' and their VC increments to [1,0,0].
-    -- Alice's message is conveyed by transport.
-    modifyIORef alice $ broadcast "I lost my wallet..."
-    aliceBcastLost <- atomicModifyIORef alice convey
+    mLost <- atomicModifyIORef alice $ swap . broadcast "I lost my wallet..."
 
     -- Alice sends 'found' and their VC increments to [2,0,0].
-    -- Alice's message is conveyed by transport.
-    modifyIORef alice $ broadcast "Found it!"
-    aliceBcastFound <- atomicModifyIORef alice convey
+    mFound <- atomicModifyIORef alice $ swap . broadcast "Found it!"
 
     -- Carol receives 'found' and delays it because it depends on 'lost'.
-    modifyIORef carol $ \p -> foldr receive p aliceBcastFound
-    Nothing <- atomicModifyIORef carol deliver
+    modifyIORef carol $ receive mFound
+    Nothing <- modifyIORefMaybe carol $ fmap swap . deliver
 
     -- Carol receives 'lost' and delivers it, updating their VC to [1,0,0].
-    modifyIORef carol $ \p -> foldr receive p aliceBcastLost
-    Just (Message{mRaw="I lost my wallet..."}) <- atomicModifyIORef carol deliver
+    modifyIORef carol $ receive mLost
+    Just Message{mRaw="I lost my wallet..."} <- modifyIORefMaybe carol $ fmap swap . deliver
 
     -- Carol delivers 'found', updating their VC to [2,0,0]
-    Just (Message{mRaw="Found it!"}) <- atomicModifyIORef carol deliver
+    Just Message{mRaw="Found it!"} <- modifyIORefMaybe carol $ fmap swap . deliver
 
-    return ()
+    print =<< readIORef alice
+    print =<< readIORef bob
+    print =<< readIORef carol
 
 
 rightExample :: IO ()
 rightExample = do
     -- Three causal broadcast processes which send String messages.
-    alice <- newIORef (pNew 0 3 :: Process String)
-    bob   <- newIORef (pNew 1 3 :: Process String)
-    carol <- newIORef (pNew 2 3 :: Process String)
+    let n = 3
+    alice <- newIORef (pEmpty n 0 :: P String)
+    bob   <- newIORef (pEmpty n 1 :: P String)
+    carol <- newIORef (pEmpty n 2 :: P String)
 
     -- Alice sends 'lost' and their VC increments to [1,0,0].
-    -- Alice's message is conveyed by transport.
-    modifyIORef alice $ broadcast "I lost my wallet..."
-    aliceBcastLost <- atomicModifyIORef alice convey
+    mLost <- atomicModifyIORef alice $ swap . broadcast "I lost my wallet..."
 
     -- Alice sends 'found' and their VC increments to [2,0,0].
-    -- Alice's message is conveyed by transport.
-    modifyIORef alice $ broadcast "Found it!"
-    aliceBcastFound <- atomicModifyIORef alice convey
+    mFound <- atomicModifyIORef alice $ swap . broadcast "Found it!"
 
     -- Bob receives both 'lost' and 'found' and delivers them in causal order,
     -- updating their VC to [2,0,0].
-    modifyIORef bob $ \p -> foldr receive p (aliceBcastLost ++ aliceBcastFound)
-    Just (Message{mRaw="I lost my wallet..."}) <- atomicModifyIORef bob deliver
-    Just (Message{mRaw="Found it!"}) <- atomicModifyIORef bob deliver
+    modifyIORef bob $ receive mFound
+    modifyIORef bob $ receive mLost
+    Just (Message{mRaw="I lost my wallet..."}) <- modifyIORefMaybe bob $ fmap swap . deliver
+    Just (Message{mRaw="Found it!"}) <- modifyIORefMaybe bob $ fmap swap . deliver
 
     -- Carol receives 'lost' and delivers it, updating their VC to [1,0,0].
-    modifyIORef carol $ \p -> foldr receive p aliceBcastLost
-    Just (Message{mRaw="I lost my wallet..."}) <- atomicModifyIORef carol deliver
+    modifyIORef carol $ receive mLost
+    Just (Message{mRaw="I lost my wallet..."}) <- modifyIORefMaybe carol $ fmap swap . deliver
 
     -- Bob sends 'glad' and their VC increments to [2,1,0].
-    -- Bob's message is conveyed by transport.
-    modifyIORef bob $ broadcast "Glad to hear it!"
-    bobBcastGlad <- atomicModifyIORef bob convey
+    mGlad <- atomicModifyIORef bob $ swap . broadcast "Glad to hear it!"
 
     -- Carol receives 'glad' and delays it because it depends on 'found'.
-    modifyIORef carol $ \p -> foldr receive p bobBcastGlad
-    Nothing <- atomicModifyIORef carol deliver
+    modifyIORef carol $ receive mGlad
+    Nothing <- modifyIORefMaybe carol $ fmap swap . deliver
 
     -- Carol receives 'found' and delivers it, updating their VC to [2,0,0].
-    modifyIORef carol $ \p -> foldr receive p aliceBcastFound
-    Just (Message{mRaw="Found it!"}) <- atomicModifyIORef carol deliver
+    modifyIORef carol $ receive mFound
+    Just (Message{mRaw="Found it!"}) <- modifyIORefMaybe carol $ fmap swap . deliver
 
     -- Carol delivers 'glad', updating their VC to [2,1,0].
-    Just (Message{mRaw="Glad to hear it!"}) <- atomicModifyIORef carol deliver
+    Just (Message{mRaw="Glad to hear it!"}) <- modifyIORefMaybe carol $ fmap swap . deliver
 
-    return ()
+    print =<< readIORef alice
+    print =<< readIORef bob
+    print =<< readIORef carol
