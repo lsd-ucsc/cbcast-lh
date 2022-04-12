f58fe30dd9c6ec62c25e4f0d332ec8e19b641627
PLR <51248199+plredmond@users.noreply.github.com>
2022-04-12 13:32:16 -0700

    debugscript starts a flask server binding the host:port specified in kv-store-args, and after 3sec attempts to hit the node-hostports and node-ipports

--- deploy/host-kv.nix
+++ deploy/host-kv.nix
@@ -14,10 +14,38 @@ let
   node-ipports = map (nn: "${get-ip nodes.${nn}.config}:${toString kv-store-port}") node-names;
   node-hostports = map (nn: "${nn}:${toString kv-store-port}") node-names; # nixops populates the hosts file with hostnames
   kv-store-args = "${toString kv-store-offset} ${builtins.concatStringsSep " " node-ipports}";
+  # debugscript starts a flask server binding the host:port specified in
+  # kv-store-args, and after 3sec attempts to hit the node-hostports and
+  # node-ipports
   debugScript = ''
-    echo example ${kv-store-args}
-    echo hostports ${toString node-hostports}
-    echo ipports ${toString node-ipports}
+    #!${pkgs.python3.withPackages (p: [p.flask p.requests])}/bin/python
+
+    import sys
+    import flask
+    import requests
+    import threading
+    log = lambda *args, **kwargs: print(*args, flush=True, **kwargs)
+
+    kvStoreArgs = """${toString kv-store-args}""".split()
+    hostPorts = """${toString node-hostports}""".split()
+    ipPorts = """${toString node-ipports}""".split()
+
+    pid = int(kvStoreArgs.pop(0))
+    assert len(kvStoreArgs) == len(hostPorts) == len(ipPorts)
+
+    def background():
+        for hp in hostPorts + ipPorts:
+            try:
+                requests.head(f'http://{hp}')
+            except Exception as e:
+                log(f'[FAIL] {hp} {e.__class__.__name__}')
+            else:
+                log(f'[OKAY] {hp}')
+
+    if __name__ == '__main__':
+        threading.Timer(3, background).start()
+        host, _, port = kvStoreArgs[pid].partition(':')
+        flask.Flask(__name__).run(host=host, port=port)
   '';
 in
 {
@@ -32,7 +60,7 @@ in
     serviceConfig = {
       ExecStart =
         if skip-build
-        then "${pkgs.bash}/bin/bash ${pkgs.writeText "debug.sh" debugScript}"
+        then pkgs.writeScript "debug.py" debugScript
         else "${(import ../default.nix).default}/bin/example ${kv-store-args}";
     };
   };
