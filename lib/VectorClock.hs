3d805e06ce531e6483f42f8bd5d63928bfb1d053
PLR <51248199+plredmond@users.noreply.github.com>
2022-04-19 12:11:38 -0700

    make VC not reliant on system model

--- lib/VectorClock.hs
+++ lib/VectorClock.hs
@@ -3,7 +3,6 @@
 module VectorClock where
 
 import Redefined
-import MessagePassingAlgorithm
 
 
 
@@ -15,21 +14,20 @@ import MessagePassingAlgorithm
 type Clock = {c:Integer | 0 <= c} @-}
 type Clock = Integer
 
--- | A vector clock maps PID to Clock.
+-- | A vector clock maps an index (process identifier) to a Clock.
 {-@
 type VC = [Clock] @-}
 type VC = [Clock]
 {-@ type VCsized N = {v:VC | len v == N} @-}
 {-@ type VCasV V = VCsized {len V} @-}
 
-{-@ type PIDasV V = PIDsized {len V} @-}
-
--- QQQ: everywhere a *asV type is defined, we call len, but perhaps we should
--- alias that here to vcSize
---
--- QQQ: similarly, everywhere we deal with proccount we specify Nat on the LH
--- side and Int on the haskell side; perhaps we should have a type alias here
--- (or up by PID?)
+--- QQQ: everywhere an *asV type is defined, we call len, but perhaps we should
+--- alias that here to vcSize
+---
+--- QQQ: similarly, everywhere we deal with proccount we specify Nat on the LH
+--- side and Int on the haskell side; perhaps we should have a type alias here
+---
+--- QQQ: perhaps vcEmpty should be in terms of a Redefined.replicate
 
 
 
@@ -77,8 +75,8 @@ vcConcurrent a b = not (vcLess a b) && not (vcLess b a)
 
 -- | Increment the ith offset into the VC (i=0 increments head).
 {-@
-vcTick :: v:VC -> PIDasV {v} -> VCasV {v} @-}
-vcTick :: VC -> PID -> VC
+vcTick :: v:VC -> {i:Nat | i < len v} -> VCasV {v} @-}
+vcTick :: VC -> Int -> VC
 vcTick (x:xs) 0 = (x + 1) : xs
 vcTick (x:xs) i = x : vcTick xs (i - 1)
 {-@ reflect vcTick @-}
