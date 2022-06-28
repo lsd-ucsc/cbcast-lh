533874d36ff118db85584fc21d8c8283e8acbeae
PLR <51248199+plredmond@users.noreply.github.com>
2022-06-28 12:05:57 -0700

    have the permissive network-semantics use CD as its core

--- lib/CBCAST/Verification/XStep.hs
+++ lib/CBCAST/Verification/XStep.hs
@@ -2,40 +2,16 @@
 {-# OPTIONS_GHC "-Wno-warnings-deprecations" #-} -- Hide the "verification only" and "internal use" warnings
 {-# LANGUAGE GADTs #-}
 
-module CBCAST.Verification.NetworkSemantics2 {-# WARNING "Verification only" #-} where
+-- | Permissive network-semantics for executions running CBCAST.
+module CBCAST.Verification.XStep {-# WARNING "Verification only" #-} where
 
 import Prelude hiding (foldr, uncurry)
 
-import Redefined
+-- import Redefined
 import CBCAST.Core
 import CBCAST.Step
-import CBCAST.Transitions
-import CBCAST.Verification.PLCDpresStep (stepShim)
-
-
-
-
--- * Execution
-
--- | An execution is a mapping from process identifier to CBCAST processes. The
--- mapping is constrained to only those processes /in/ the execution by the
--- size parameter, @N@.
-type Execution r = PID -> Process r
-{-@ type Xsized r N = p_id_k:PIDsized {N} -> {p_v:Psized r {N} | p_id_k == pID p_v} @-}
-
-{-@
-setProcess :: n:Nat -> p_id:PIDsized {n} -> {p:Psized r {n} | p_id == pID p} -> Xsized r {n} -> Xsized r {n} @-}
-setProcess :: Int -> PID -> Process r -> Execution r -> Execution r
-setProcess _n k v mapping target
-    | target == k = v
-    | otherwise = mapping target
-{-@ reflect setProcess @-}
-
-{-@
-setProcess2 :: n:Nat -> Psized r {n} -> Xsized r {n} -> Xsized r {n} @-}
-setProcess2 :: Int -> Process r -> Execution r -> Execution r
-setProcess2 n p = setProcess n (pID p) p
-{-@ reflect setProcess2 @-}
+-- import CBCAST.Transitions
+-- import CBCAST.Verification.PLCDpresStep (stepShim)
 
 
 
@@ -71,6 +47,3 @@ foldr_xStep :: n:Nat -> Xsized r {n} -> [(OPsized r {n}, PIDsized {n})] -> Xsize
 foldr_xStep :: Int -> Execution r -> [(Op r, PID)] -> Execution r
 foldr_xStep n x = foldr (uncurry (xStep n)) x
 {-@ reflect foldr_xStep @-}
-
-
-
