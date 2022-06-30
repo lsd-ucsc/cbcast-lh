2f1b8618eae2ea5a9d30ba9f4f8fc80da5b7401f
PLR <51248199+plredmond@users.noreply.github.com>
2022-06-30 13:36:06 -0700

    make Global module use Subject-Verb-Object

--- lib/CBCAST/Verification/Global/XStepPresCD.hs
+++ lib/CBCAST/Verification/Global/XStepPresCD.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC "-Wno-unused-imports" #-} -- LH needs bodies of reflected definitions
 {-# OPTIONS_GHC "-Wno-warnings-deprecations" #-} -- Hide the "verification only" and "internal use" warnings
 
-module CBCAST.Verification.Global.CDpresXStep {-# WARNING "Verification only" #-} where
+module CBCAST.Verification.Global.XStepPresCD {-# WARNING "Verification only" #-} where
 
 import Language.Haskell.Liquid.ProofCombinators
 
@@ -12,8 +12,8 @@ import CBCAST.Step
 import CBCAST.Verification.ProcessOrder
 import CBCAST.Verification.PLCDpresStep
 import CBCAST.Verification.Global.Core
-import CBCAST.Verification.Global.Step
-import CBCAST.Verification.Global.PLCDpresXStep
+import CBCAST.Verification.Global.XStep
+import CBCAST.Verification.Global.XStepPresPLCD
 
 -- | An operation OP preserves any execution's observation of CD.
 {-@
@@ -24,14 +24,14 @@ type CDpreservation r N OP
 @-}
 
 {-@
-xStepCDpres :: n:Nat -> op:OPsized r {n} -> op_p_id:PIDsized {n} -> CDpreservation r {n} {xStep n op op_p_id} @-}
-xStepCDpres :: Eq r => Int -> Op r -> PID -> Execution r -> (PID -> Message r -> Message r -> Proof)
+xStepPresCD :: n:Nat -> op:OPsized r {n} -> op_p_id:PIDsized {n} -> CDpreservation r {n} {xStep n op op_p_id} @-}
+xStepPresCD :: Eq r => Int -> Op r -> PID -> Execution r -> (PID -> Message r -> Message r -> Proof)
                                                          -> (PID -> Message r -> Message r -> Proof)
-xStepCDpres n op op_p_id x xCD = -- \ p_id m₁ m₂ ->
+xStepPresCD n op op_p_id x xCD = -- \ p_id m₁ m₂ ->
     let
     x' = xStep n op op_p_id x -- restate transition, then step x
     xPLCD = cdToPLCD n x xCD
-    x'PLCD = xStepPLCDpres n op op_p_id x xPLCD
+    x'PLCD = xStepPresPLCD n op op_p_id x xPLCD
     x'CD = plcdToCD n x' x'PLCD
     in
     x'CD
--- lib/CBCAST/Verification/Global/XStepPresPLCD.hs
+++ lib/CBCAST/Verification/Global/XStepPresPLCD.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC "-Wno-unused-imports" #-} -- LH needs bodies of reflected definitions
 {-# OPTIONS_GHC "-Wno-warnings-deprecations" #-} -- Hide the "verification only" and "internal use" warnings
 
-module CBCAST.Verification.Global.PLCDpresXStep {-# WARNING "Verification only" #-} where
+module CBCAST.Verification.Global.XStepPresPLCD {-# WARNING "Verification only" #-} where
 
 import Language.Haskell.Liquid.ProofCombinators
 
@@ -15,19 +15,19 @@ import CBCAST.Verification.PLCD
 import CBCAST.Verification.PLCDpresStep
 import CBCAST.Verification.PIDpresStep
 import CBCAST.Verification.Global.Core
-import CBCAST.Verification.Global.Step
+import CBCAST.Verification.Global.XStep
 
 {-@
-xStepPLCDpres
+xStepPresPLCD
     :: n:Nat -> op:OPsized r {n} -> op_p_id:PIDsized {n} -> x:Xsized r {n}
     -> ( p_id:PIDsized {n} -> PLCD r {n}                    {x p_id} )
     -> ( p_id:PIDsized {n} -> PLCD r {n} {xStep n op op_p_id x p_id} )
 @-}
-xStepPLCDpres :: Eq r
+xStepPresPLCD :: Eq r
     => Int -> Op r -> PID -> Execution r
     -> (PID -> Message r -> Message r -> Proof)
     -> (PID -> Message r -> Message r -> Proof)
-xStepPLCDpres n op op_p_id x xPLCD {-x'PLCD λ-} p_id m₁ m₂
+xStepPresPLCD n op op_p_id x xPLCD {-x'PLCD λ-} p_id m₁ m₂
   | op_p_id == p_id =
     let
     -- global to local, local step
